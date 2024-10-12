083243f5e7ef6823c0e7cd9967d54ede10123a29
Cayden <caydenlincke4@gmail.com>
2024-10-12 13:32:41 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$page_title = "Home";
+include 'includes/header.php';
+?>
+
+<h2>Welcome to Sevvex Forum</h2>
+<p>Join our community and start discussing!</p>
+
+<?php include 'includes/footer.php'; ?>
